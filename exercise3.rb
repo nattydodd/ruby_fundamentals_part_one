3abaa6ec46c01d688b083003976f9e6b95e37313
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-09 15:46:57 -0400

    Calculating age considering birth date

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,16 @@ name = gets.chomp
 #Saves the users name as a variable and gets rid of white space after"
 
 puts "Hi #{name}!"
+
+#Obtaining the user's age"
+puts "How old are you?"
+age = gets.chomp
+puts "Has your birthday already come this year?"
+answer = gets.chomp
+
+birth_year = ( 2016 - age.to_i )
+if answer == "yes"
+  puts "You were born in #{birth_year}!"
+else
+  puts "You were born in " + (birth_year - 1).to_s + "!"
+end
