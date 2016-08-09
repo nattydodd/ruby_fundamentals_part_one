a718944d100338e9c71c57fb4f31f3d8e0616c6a
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-09 14:54:47 -0400

    Question 2: Adding integers and strings

--- exercise2.rb
+++ exercise2.rb
@@ -1,4 +1,4 @@
-#How would you calculate a tip fr a 55 dollar meal?
+#1. How would you calculate a tip fr a 55 dollar meal?
 tip = 8
 meal_price = 55
 
@@ -9,3 +9,14 @@ elsif tip >= (meal_price * 0.20)
 else
   puts "You might want to start running..."
 end
+
+#2. Add a string and an integer with the + operator. Then convert the integer into a string.
+
+# "My age is" + 27
+# this results in an error
+
+puts "My age is " + 27.to_s
+
+#alternate answer:
+age = 27
+puts "My age is #{age}"
