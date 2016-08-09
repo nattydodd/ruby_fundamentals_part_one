bc07488ad5b6b14e051941eed2222c4b2b518b8a
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-09 14:59:18 -0400

    Question 3: Outputting into a string

--- exercise2.rb
+++ exercise2.rb
@@ -20,3 +20,8 @@ puts "My age is " + 27.to_s
 #alternate answer:
 age = 27
 puts "My age is #{age}"
+
+#3. Try outputting the result of 45628 x 7839 in a sentence using string interpolation.
+
+result = 45628 * 7839
+puts "The result is #{result}"
