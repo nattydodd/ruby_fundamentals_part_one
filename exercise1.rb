e3bfbe2e825b531d9d7d10a0a90b0bc51631b5c7
Natalie Dodd <nataliedodd89@gmail.com>
2016-08-09 14:19:16 -0400

    Escape sequences and Booleans

--- exercise1.rb
+++ exercise1.rb
@@ -1,3 +1,28 @@
 puts 2
 puts 3
 puts 2 + 3
+
+puts "Betty's pie shop"
+puts 'Betty\'s pie shop'
+
+#Common Escape Sequences:
+# \" double quote
+#
+# \\ single backslash
+#
+# \a bell/alert
+#
+# \b backspace
+#
+# \r carriage return
+#
+# \n newline
+#
+# \s space
+#
+# \t tab
+
+puts "Hello\t\tworld"
+puts "Hello\b\b\b\bGoodbye world"
+puts "Hello\rStart over world"
+puts "1. Hello\n2. World"
